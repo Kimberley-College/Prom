fc8737b9b2edbc149a3524aa36c9d59e927aa34b
Nick <hello@nicholasg.me>
2022-02-16 18:45:40 +0000

    chore(lint): fix linting

--- .eslintrc.js
+++ .eslintrc.js
@@ -3,47 +3,40 @@ module.exports = {
   env: {
     node: true,
     es6: true,
+    browser: true,
   },
-  parserOptions: { 
-    ecmaVersion: 8,  // to enable features such as async/await
-    project: './tsconfig.json' // connct to tsconfig
-   },
-  ignorePatterns: ['node_modules/*', '.next/*', '.out/*', '!.prettierrc.js'], // We don't want to lint generated files nor node_modules, but we want to lint .prettierrc.js (ignored by default by eslint)
-  extends: ['eslint:recommended'],
-  overrides: [
-    // This configuration will apply only to TypeScript files
-    {
-      files: ['**/*.ts', '**/*.tsx'],
-      parser: '@typescript-eslint/parser',
-      settings: { react: { version: 'detect' } },
-      env: {
-        browser: true,
-        node: true,
-        es6: true,
-      },
-      extends: [
-        'eslint:recommended',
-        'plugin:@typescript-eslint/recommended', // TypeScript rules
-        'plugin:react/recommended', // React rules
-        'plugin:react-hooks/recommended', // React hooks rules
-        'plugin:jsx-a11y/recommended', // Accessibility rules
-        'airbnb-typescript',
-      ],
-      rules: {
-        'react/prop-types': 'off',
-        'react/react-in-jsx-scope': 'off',
-        'react/jsx-props-no-spreading': 'off',
-        'jsx-a11y/anchor-is-valid': 'off',
-        '@typescript-eslint/no-unused-vars': ['error'],
-        '@typescript-eslint/explicit-function-return-type': [
-          'warn',
-          {
-            allowExpressions: true,
-            allowConciseArrowFunctionExpressionsStartingWithVoid: true,
-          },
-        ],
-        'max-len': 'off',
-      },
-    },
+  parser: '@typescript-eslint/parser',
+  parserOptions: {
+    ecmaVersion: 8, // to enable features such as async/await
+    project: './tsconfig.json',
+  },
+  ignorePatterns: ['node_modules/*', '.next/*'],
+  settings: { react: { version: 'detect' } },
+  plugins: [
+    '@typescript-eslint',
   ],
-}
+  extends: [
+    'plugin:react/recommended', // React rules
+    'plugin:react-hooks/recommended', // React hooks rules
+    'plugin:jsx-a11y/recommended', // Accessibility rules
+    'plugin:@typescript-eslint/recommended', // TypeScript rules
+    'airbnb',
+    'airbnb-typescript',
+  ],
+  rules: {
+    'react/prop-types': 'off',
+    'react/react-in-jsx-scope': 'off',
+    'react/jsx-props-no-spreading': 'off',
+    'jsx-a11y/anchor-is-valid': 'off',
+    '@typescript-eslint/no-unused-vars': ['error'],
+    '@typescript-eslint/explicit-function-return-type': [
+      'warn',
+      {
+        allowExpressions: true,
+        allowConciseArrowFunctionExpressionsStartingWithVoid: true,
+      },
+    ],
+    'max-len': 'off',
+    'react/function-component-definition': 'off',
+  },
+};

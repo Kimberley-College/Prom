659ccd1d746d99f33824ef5ab38855c0cba51231
Jacques <jamie.whiting2003@gmail.com>
2022-02-17 15:54:03 +0000

    put a title on the homepage

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,9 +1,14 @@
 import type { NextPage } from 'next';
+import { Box, Text } from '@chakra-ui/react';
+import Image from 'next/image';
 
 const Home: NextPage = () => (
-  <div>
-    Home Page
-  </div>
+  <Box bg="#1A365D" w="100%" p={4} color="white" display="flex" alignItems="center">
+    <Image src="/kimberley_logo.png" height={110} width={110} alt="boioioing" />
+    <Text fontSize="35">
+      Kimberley College Prom 2022
+    </Text>
+  </Box>
 );
 
 export default Home;

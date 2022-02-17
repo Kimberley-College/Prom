ab76846be12e10c81fc20906f4fe6cebb3402768
Jacques <99771266+JacquesRW@users.noreply.github.com>
2022-02-17 18:16:14 +0000

    Implement Chakra UI (#1) (#9)
    * installed chakra-ui
    
    * put a title on the homepage
    
    * chore: bump next
    
    Co-authored-by: Nick <hello@nicholasg.me>

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,8 +1,11 @@
 import type { AppProps } from 'next/app';
 import type { NextPage } from 'next';
+import { ChakraProvider } from '@chakra-ui/react';
 
 const App: NextPage<AppProps> = ({ Component, pageProps }) => (
-  <Component {...pageProps} />
+  <ChakraProvider>
+    <Component {...pageProps} />
+  </ChakraProvider>
 );
 
 export default App;

efdb911fe4983c26001db2ac6d0e5f9aa8bf2e34
Jacques <jamie.whiting2003@gmail.com>
2022-02-19 01:41:28 +0000

    some formatting + issue daddy Nick is fixing

--- src/components/Layouts/Base.tsx
+++ src/components/Layouts/Base.tsx
@@ -1,13 +1,14 @@
-import { Flex, Box } from '@chakra-ui/react';
+import { Flex, Box, Spacer } from '@chakra-ui/react';
 import NavBar from '../Nav/NavBar';
 import Footer from './Footer';
 
 const BaseLayout: React.FC = ({ children }) => (
-  <Flex flexFlow="column nowrap" w="100%" maxW="100%" minH="100vh" boxSizing="border-box" bgColor="#EDF2F7">
+  <Flex flexFlow="column nowrap" w="100%" maxW="100%" minH="100vh" boxSizing="border-box" bgColor="gray.100">
     <Box pos="absolute" w="100%" zIndex={100}>
       <NavBar />
     </Box>
     {children}
+    <Spacer />
     <Footer />
   </Flex>
 );
--- src/components/Layouts/Footer.tsx
+++ src/components/Layouts/Footer.tsx
@@ -3,14 +3,16 @@ import {
 } from '@chakra-ui/react';
 
 const Footer: React.FC = () => (
-  <Flex borderTopStyle="dashed" borderTopWidth="1px" direction="row" px="5vw" py={3} alignItems="center">
+  <Flex borderTopStyle="dashed" borderTopWidth="1px" direction="row" px="5vw" py={3} alignItems="center" bgGradient="linear(to-b,gray.100,gray.300)">
     <Box w="40vw">
       Special Thanks: Inferno Communications, Josh Heng, Nick, Jacques, Jean, Dustin
     </Box>
     <Spacer />
-    <Text>
-      <Link>Terms and Conditions</Link> | <Link>Privacy Policy</Link>
-    </Text>
+    <Flex justify="right" w="40vw">
+      <Text>
+        <Link>Terms and Conditions</Link>  |  <Link>Privacy Policy</Link>
+      </Text>
+    </Flex>
   </Flex>
 );
 

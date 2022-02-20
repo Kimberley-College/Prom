b8562729aaaaba998e15e370be06bdba6c4543c2
Nick <hello@nicholasg.me>
2022-02-20 21:20:46 +0000

    *feat: stripe webhooks implemented *chore: stripe now creates customer object adds name/email to it *chore: now add a cookie for Supabase auth so that it can be used in api routes

--- src/pages/api/stripe/webhook.ts
+++ src/pages/api/stripe/webhook.ts
@@ -1,3 +1,73 @@
-import type { NextApiRequest, NextApiResponse } from 'next';
+import { buffer } from 'micro';
+import Cors from 'micro-cors';
+import { NextApiRequest, NextApiResponse } from 'next';
 
-export default async (req: NextApiRequest, res: NextApiResponse): Promise<NextApiResponse> => res.status(200);
+import Stripe from 'stripe';
+
+const stripe = new Stripe(process.env.STRIPE_SECRET_KEY, {
+  apiVersion: '2020-08-27',
+});
+
+const webhookSecret = process.env.STRIPE_WEBHOOK_SECRET;
+
+// Stripe requires the raw body to construct the event.
+export const config = {
+  api: {
+    bodyParser: false,
+  },
+};
+
+const cors = Cors({
+  allowMethods: ['POST', 'HEAD'],
+});
+
+const webhookHandler = async (req: NextApiRequest, res: NextApiResponse): Promise<void> => {
+  if (req.method !== 'POST') {
+    res.setHeader('Allow', 'POST');
+    res.status(405).end('Method Not Allowed');
+    return;
+  }
+
+  const buf = await buffer(req);
+  const sig = req.headers['stripe-signature'];
+
+  let event: Stripe.Event;
+
+  try {
+    event = stripe.webhooks.constructEvent(buf.toString(), sig, webhookSecret);
+  } catch (err) {
+    const errorMessage = err instanceof Error ? err.message : 'Unknown error';
+    // On error, log and return the error message.
+    if (err instanceof Error) console.log(err);
+    console.log(`❌ Error message: ${errorMessage}`);
+    res.status(400).send(`Webhook Error: ${errorMessage}`);
+    return;
+  }
+
+  let paymentIntent: Stripe.PaymentIntent;
+  switch (event.type) {
+    case 'payment_intent.succeeded':
+      paymentIntent = event.data.object as Stripe.PaymentIntent;
+      break;
+    case 'payment_intent.payment_failed':
+      paymentIntent = event.data.object as Stripe.PaymentIntent;
+      break;
+    default:
+      console.warn(`Unhandled event type: ${event.type}`);
+      return;
+  }
+
+  const customer = await stripe.customers.create({
+    name: paymentIntent.metadata.name,
+    email: paymentIntent.metadata.email,
+  });
+
+  await stripe.paymentIntents.update(paymentIntent.id, {
+    customer: customer.id,
+  });
+
+  // Acknowledge code
+  res.json({ received: true });
+};
+
+export default cors(webhookHandler);
--- src/pages/checkout.tsx
+++ src/pages/checkout.tsx
@@ -1,8 +1,9 @@
 import CheckoutWrapper from 'components/Checkout/CheckoutWrapper';
 import BaseLayout from 'components/Layouts/Base';
 import { Box, Flex } from '@chakra-ui/react';
+import type { NextPage } from 'next';
 
-const Checkout = () => (
+const Checkout: NextPage = () => (
   <BaseLayout>
     <Flex flexFlow="column nowrap" justify="center" align="center">
       <Box minW="300px" maxW="500px" w="70%">

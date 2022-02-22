38db9c8fcab9d4a0f5106ab313a2a21d6b97922b
Nick <33908215+NicholasG04@users.noreply.github.com>
2022-02-22 18:27:27 +0000

    Create PWA and SEO (#27)
    * chore: install next pwa
    
    * chore: add manifest
    
    * chore: add seo
    
    * chore: add sw to gitignore
    * chore: add test icons
    * chore: update manifest
    * chore: add required links/meta tags for PWA

--- next.config.js
+++ next.config.js
@@ -1,3 +1,9 @@
-module.exports = {
+/* eslint-disable @typescript-eslint/no-var-requires */
+const withPWA = require('next-pwa');
+
+module.exports = withPWA({
   poweredByHeader: false,
-};
+  pwa: {
+    dest: 'public',
+  },
+});
--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,6 +1,7 @@
 import type { AppProps } from 'next/app';
 import type { NextPage } from 'next';
 import { ChakraProvider } from '@chakra-ui/react';
+import { DefaultSeo } from 'next-seo';
 import supabase from '../util/supabaseClient';
 import theme from '../util/theme';
 import { UserContextProvider } from '../util/useUser';
@@ -8,6 +9,56 @@ import { UserContextProvider } from '../util/useUser';
 const App: NextPage<AppProps> = ({ Component, pageProps }) => (
   <UserContextProvider supabaseClient={supabase}>
     <ChakraProvider theme={theme}>
+      <DefaultSeo
+        title="Kimberley College Prom"
+        description="A website for the Kimberley College Prom 2022"
+        openGraph={{
+          title: 'Kimberley College Prom',
+          description: 'A website for the Kimberley College Prom 2022',
+          type: 'website',
+          locale: 'en_GB',
+          url: 'https://prom.kim',
+          site_name: 'Kimberley College Prom',
+        }}
+        additionalLinkTags={[
+          {
+            rel: 'shortcut icon',
+            type: 'image/png',
+            href: '/favicon.ico',
+          },
+          {
+            rel: 'manifest',
+            href: '/manifest.json',
+          },
+          {
+            rel: 'apple-touch-icon',
+            sizes: '180x180',
+            href: '/icons/apple-touch-icon.png',
+          },
+        ]}
+        additionalMetaTags={[
+          {
+            name: 'application-name',
+            content: 'Kimberley College Prom',
+          },
+          {
+            name: 'apple-mobile-web-app-title',
+            content: 'Kiberley College Prom',
+          },
+          {
+            name: 'apple-mobile-web-app-capable',
+            content: 'yes',
+          },
+          {
+            name: 'apple-mobile-web-app-status-bar-style',
+            content: 'default',
+          },
+          {
+            name: 'mobile-web-app-capable',
+            content: 'yes',
+          },
+        ]}
+      />
       <Component {...pageProps} />
     </ChakraProvider>
   </UserContextProvider>

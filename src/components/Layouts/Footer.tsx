5013168c66c1dfa0d0c188a2ef843e603a2b8a45
Jacques <99771266+JacquesRW@users.noreply.github.com>
2022-02-22 23:38:07 +0000

    made privacy and terms files and footer links (#30)
    * made privacy and terms files and footer links
    
    * removed border from footer
    
    * chore: add privacy policy
    
    * chore: add markdown renderer
    
    * chore: start memoising, but breaks rules of hooks - must fix
    
    * chore: add basic terms
    
    * chore: add basic terms of sale
    
    * chore: add link to terms and privacy on checkout
    
    Co-authored-by: Nick <33908215+NicholasG04@users.noreply.github.com>
    Co-authored-by: Nick <hello@nicholasg.me>

--- src/components/Layouts/Footer.tsx
+++ src/components/Layouts/Footer.tsx
@@ -1,16 +1,17 @@
 import {
   Box, Flex, Spacer, Text, Link,
 } from '@chakra-ui/react';
+import NextLink from 'next/link';
 
 const Footer: React.FC = () => (
-  <Flex borderTopStyle="dashed" borderTopWidth="1px" direction="row" px="5vw" py={3} alignItems="center" bgGradient="linear(to-b,gray.100,gray.300)">
+  <Flex direction="row" px="5vw" py={3} alignItems="center" bgGradient="linear(to-b,gray.100,gray.300)">
     <Box w="40vw">
       Special Thanks: Inferno Communications, Josh Heng, Nick, Jacques, Jean, Dustin, Raffi
     </Box>
     <Spacer />
     <Flex justify="right" w="40vw">
       <Text>
-        <Link>Terms and Conditions</Link>  |  <Link>Privacy Policy</Link>
+        <NextLink href="/terms" passHref><Link>Terms and Conditions</Link></NextLink>  |  <NextLink href="/privacy" passHref><Link>Privacy Policy</Link></NextLink>
       </Text>
     </Flex>
   </Flex>

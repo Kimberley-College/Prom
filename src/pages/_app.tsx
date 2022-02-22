5f7fab68e8cba403a27cb9195b5080a6bbd8def4
Nick <hello@nicholasg.me>
2022-02-22 17:35:25 +0000

    chore: add seo

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,6 +1,7 @@
 import type { AppProps } from 'next/app';
 import type { NextPage } from 'next';
 import { ChakraProvider } from '@chakra-ui/react';
+import { DefaultSeo } from 'next-seo';
 import supabase from '../util/supabaseClient';
 import theme from '../util/theme';
 import { UserContextProvider } from '../util/useUser';
@@ -8,6 +9,25 @@ import { UserContextProvider } from '../util/useUser';
 const App: NextPage<AppProps> = ({ Component, pageProps }) => (
   <UserContextProvider supabaseClient={supabase}>
     <ChakraProvider theme={theme}>
+      <DefaultSeo
+        title="Kimberley College Prom"
+        description="A website for the Kimberley College Prom 2022"
+        openGraph={{
+          title: 'Kimberley College Prom',
+          description: 'A website for the Kimberley College Prom 2022',
+          type: 'website',
+          locale: 'en_GB',
+          url: 'https://prom.kim',
+          site_name: 'Kimberley College Prom',
+        }}
+        additionalLinkTags={[
+          {
+            rel: 'icon',
+            type: 'image/png',
+            href: '/favicon.ico',
+          },
+        ]}
+      />
       <Component {...pageProps} />
     </ChakraProvider>
   </UserContextProvider>

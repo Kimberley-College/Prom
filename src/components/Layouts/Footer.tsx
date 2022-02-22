787c72ae49a43775b7e99dd5a166d93c86524aa5
John Montgomery <64652231+john-montgomery2003@users.noreply.github.com>
2022-02-22 11:13:48 +0000

    Update Footer.tsx (#45)

--- src/components/Layouts/Footer.tsx
+++ src/components/Layouts/Footer.tsx
@@ -5,7 +5,7 @@ import {
 const Footer: React.FC = () => (
   <Flex borderTopStyle="dashed" borderTopWidth="1px" direction="row" px="5vw" py={3} alignItems="center" bgGradient="linear(to-b,gray.100,gray.300)">
     <Box w="40vw">
-      Special Thanks: Inferno Communications, Josh Heng, Nick, Jacques, Jean, Dustin
+      Special Thanks: Inferno Communications, Josh Heng, Nick, Jacques, Jean (Lead Developer), Dustin, Raffi 
     </Box>
     <Spacer />
     <Flex justify="right" w="40vw">

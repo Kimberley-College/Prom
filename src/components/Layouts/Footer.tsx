a60701f98260f8f268a7a8f38ac8ad9cf761da57
Nick <33908215+NicholasG04@users.noreply.github.com>
2022-02-28 11:25:06 +0000

    chore: remove Inferno from thanks

--- src/components/Layouts/Footer.tsx
+++ src/components/Layouts/Footer.tsx
@@ -6,7 +6,7 @@ import NextLink from 'next/link';
 const Footer: React.FC = () => (
   <Flex direction="row" px="5vw" py={3} alignItems="center" bgGradient="linear(to-b,gray.100,gray.300)">
     <Box w="40vw">
-      Special Thanks: Inferno Communications, Josh Heng, Nick, Jacques, Jean, Dustin, Raffi
+      Developers/Thanks: Nick, Jacques, Jean, Dustin, Raffi, Josh Heng
     </Box>
     <Spacer />
     <Flex justify="right" w="40vw">

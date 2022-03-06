dfa48f4552e44d998522ada95ce27bf76a6ee6de
Nick <33908215+NicholasG04@users.noreply.github.com>
2022-03-06 21:03:11 +0000

    refactor: start scanner/qr refactor (#89)
    * refactor: start scanner/qr refactor
    
    * refactor: generate UUIDs and times on client
    
    * fix: various webhook jwt bugs
    
    * fix: don't keep reqeusting ticket after we've found it
    
    * fix: checking in tickets
    
    * fix: using non inverted qr code so it actually works

--- src/pages/admin/scanner.tsx
+++ src/pages/admin/scanner.tsx
@@ -1,62 +1,19 @@
-import { QrReader } from 'react-qr-reader';
-import type { OnResultFunction } from 'react-qr-reader';
 import { Flex, Heading, useToast } from '@chakra-ui/react';
 import BaseLayout from 'components/Layouts/Base';
-
-const ViewFinder = () => (
-  <svg
-    viewBox="0 0 100 100"
-    style={{
-      top: 0,
-      left: 0,
-      zIndex: 1,
-      boxSizing: 'border-box',
-      border: '50px solid rgba(0, 0, 0, 0.3)',
-      position: 'absolute',
-      width: '100%',
-      height: '100%',
-    }}
-  >
-    <path
-      fill="none"
-      d="M13,0 L0,0 L0,13"
-      stroke="rgba(255, 0, 0, 0.5)"
-      strokeWidth="5"
-    />
-    <path
-      fill="none"
-      d="M0,87 L0,100 L13,100"
-      stroke="rgba(255, 0, 0, 0.5)"
-      strokeWidth="5"
-    />
-    <path
-      fill="none"
-      d="M87,100 L100,100 L100,87"
-      stroke="rgba(255, 0, 0, 0.5)"
-      strokeWidth="5"
-    />
-    <path
-      fill="none"
-      d="M100,13 L100,0 87,0"
-      stroke="rgba(255, 0, 0, 0.5)"
-      strokeWidth="5"
-    />
-  </svg>
-);
+import QRScanner from 'components/Scanner/QRScanner';
 
 const Scanner: React.FC = () => {
   const toast = useToast();
 
-  const checkInTicket: OnResultFunction = async (result) => {
+  const checkInTicket = async (result: string) => {
     if (!result) return;
-    const ticket = result.getText();
 
     const res = await fetch('/api/tickets/checkIn', {
       method: 'POST',
       headers: {
         'Content-Type': 'application/json',
       },
-      body: JSON.stringify({ ticket }),
+      body: JSON.stringify({ ticket: result }),
     });
 
     if (!res.ok) {
@@ -80,15 +37,7 @@ const Scanner: React.FC = () => {
     <BaseLayout>
       <Heading as="h1" size="3xl" textAlign="center" my={5}>Ticket Scanner</Heading>
       <Flex direction="column" justify="center" align="center" w="100%">
-        <QrReader
-          containerStyle={{ width: '500px', maxWidth: '80vw', height: '500px' }}
-          videoStyle={{
-            objectFit: 'cover',
-          }}
-          constraints={{ facingMode: 'environment' }}
-          onResult={checkInTicket}
-          ViewFinder={ViewFinder}
-        />
+        <QRScanner onSuccess={checkInTicket} onError={console.log} verbose={false} disableFlip />
       </Flex>
     </BaseLayout>
   );
---
+++ src/util/cookieOptions.ts
@@ -0,0 +1,7 @@
+export default {
+  name: 'sb',
+  lifetime: 60 * 60 * 24 * 30, // 30 Day Sessions
+  domain: '',
+  path: '/',
+  sameSite: 'Strict',
+};

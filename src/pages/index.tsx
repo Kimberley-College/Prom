679f2d39d105998d70b45853cc3dcc008f5094f2
Jacques <99771266+JacquesRW@users.noreply.github.com>
2022-03-07 22:15:50 +0000

    Feat/toastonloginerror (#105)
    * id like to raise a toast to daddy nick
    
    * no comment
    
    * added graphic detail to the errors

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -2,13 +2,29 @@ import type { NextPage } from 'next';
 import Header from 'components/Home/Header';
 import Cards from 'components/Home/Cards';
 import HomeLayout from 'components/Layouts/Home';
+import { useRouter } from 'next/router';
+import { useEffect } from 'react';
+import { useToast } from '@chakra-ui/react';
 
-const Home: NextPage = () => (
-  <HomeLayout>
-    <Header />
+const Home: NextPage = () => {
+  const router = useRouter();
+  const toast = useToast();
+  useEffect(() => {
+    const { query } = router;
+    if (query.error) {
+      toast({
+        status: 'error',
+        title: query.error,
+        description: query.error_description,
+      });
+    }
+  }, [router, toast]);
+  return (
+    <HomeLayout>
+      <Header />
 
-    <Cards />
-  </HomeLayout>
-);
-
+      <Cards />
+    </HomeLayout>
+  );
+};
 export default Home;

0a16d9d4a649ff8724052423bad4317448d377f2
Nick <hello@nicholasg.me>
2022-03-07 00:17:07 +0000

    fix: check authorisation on webhook

--- src/pages/api/tickets/webhook.ts
+++ src/pages/api/tickets/webhook.ts
@@ -1,6 +1,7 @@
 import { withSentry } from '@sentry/nextjs';
 import { MessageBuilder, Webhook } from 'webhook-discord';
 import { verify } from 'async-jsonwebtoken';
+import type { NextApiRequest, NextApiResponse } from 'next';
 
 interface Payload {
   type: 'INSERT'
@@ -26,10 +27,12 @@ interface JWT {
   created_at: string;
 }
 
-export default withSentry(async (req, res) => {
+export default withSentry(async (req: NextApiRequest, res: NextApiResponse<string | void>): Promise<void> => {
   const { body }: { body: Payload } = req;
 
-  const verified = verify(body.record.jwt, process.env.JWT_SECRET);
+  if (req.headers.authorization !== process.env.SUPABASE_WEBHOOK_SECRET) return res.status(403).send('Unauthorised');
+
+  const verified = await verify(body.record.jwt, process.env.JWT_SECRET);
   const jwt: JWT = verified[0] as JWT;
 
   const Hook = new Webhook(process.env.DISCORD_WEBHOOK_URL);
@@ -40,10 +43,10 @@ export default withSentry(async (req, res) => {
     .setTitle('New ticket has been purchased')
     .addField('Name', jwt.name, true)
     .addField('Email', body.record.email, true)
-    .addField('User ID', body.record.jwt, true)
+    .addField('User ID', body.record.id, true)
     .addField('Time Created', body.record.created_at, true);
 
   Hook.send(msg);
 
-  res.status(200).end();
+  return res.status(200).send('Received');
 });

36df6144b003421f10b5bbea222f374d8573365b
Jacques <jamie.whiting2003@gmail.com>
2022-03-07 22:01:13 +0000

    id like to raise a toast to daddy nick

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -2,13 +2,33 @@ import type { NextPage } from 'next';
 import Header from 'components/Home/Header';
 import Cards from 'components/Home/Cards';
 import HomeLayout from 'components/Layouts/Home';
+import { useRouter } from 'next/router';
+import { useEffect } from 'react';
+import { useToast } from '@chakra-ui/react';
 
-const Home: NextPage = () => (
-  <HomeLayout>
-    <Header />
+const Home: NextPage = () => {
+  const router = useRouter(); // from next/router
+  const toast = useToast();
+  useEffect(() => {
+    const { query } = router;
+    if (query.error) {
+      toast({
+        status: 'error',
+        title: query.error,
+        description: query.error_message, // or whatever it is''
+      });
+    }
+    // stuff here will run when the page loads
+    // parse your query params in here
+  }, [router, toast]); // dependency array = re-run when any of these change
 
-    <Cards />
-  </HomeLayout>
-);
+  // in this you might need a dependency array of [router] because you're using it inside the effect hook and effectively have to wait for it to load
+  return (
+    <HomeLayout>
+      <Header />
 
+      <Cards />
+    </HomeLayout>
+  );
+};
 export default Home;

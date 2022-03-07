5ec3f36bc1916cc2e9d20bdc045cda0f36ca5ad0
Nick <hello@nicholasg.me>
2022-03-07 20:38:44 +0000

    feat: add banner in support of ukraine

--- src/components/Layouts/Home.tsx
+++ src/components/Layouts/Home.tsx
@@ -1,10 +1,14 @@
 import { Flex, Box, Spacer } from '@chakra-ui/react';
 import NavBar from '../Nav/NavBar';
+import Banner from './Banner';
 import Footer from './Footer';
 
 const HomeLayout: React.FC = ({ children }) => (
   <Flex flexFlow="column nowrap" w="100%" maxW="100%" minH="100vh" boxSizing="border-box" bgColor="gray.100">
-    <Box pos="absolute" w="100%" zIndex={100}>
+    <Box w="100%">
+      <Banner />
+    </Box>
+    <Box pos="absolute" w="100%" zIndex={100} pt="40px">
       <NavBar />
     </Box>
     {children}

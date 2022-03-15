a495f492f7f0c372370625b19c9adf4ab046fa08
Nick <hello@nicholasg.me>
2022-03-15 22:31:46 +0000

    chore: add styling * feat: add filtering by status

--- src/components/Admin/UserCard.tsx
+++ src/components/Admin/UserCard.tsx
@@ -1,4 +1,4 @@
-import { Box } from '@chakra-ui/react';
+import { Flex, Heading, Text } from '@chakra-ui/react';
 import type { UserWithTicketInfo } from 'types/user';
 
 interface Props {
@@ -6,9 +6,12 @@ interface Props {
 }
 
 const UserCard: React.FC<Props> = ({ user }) => (
-  <Box w="300px" h="300px" bgColor="purple.300" borderRadius={10}>
-    {JSON.stringify(user)}
-  </Box>
+  <Flex w="310px" p={3} bgColor="purple.300" borderRadius={10} direction="column">
+    <Heading as="h3" size="lg" textAlign="center" pb={2}>{user.name}</Heading>
+    <Text>Email: {user.email}</Text>
+    <Text>Has Ticket: {user.has_ticket ? 'Yes' : 'No'}</Text>
+    <Text>Checked In: {user.checked_in ? 'Yes' : 'No'}</Text>
+  </Flex>
 );
 
 export default UserCard;
--- src/pages/admin/index.tsx
+++ src/pages/admin/index.tsx
@@ -1,17 +1,20 @@
 import type { NextPage } from 'next';
 import {
-  Button, Heading, Flex, useToast, Input,
+  Button, Heading, Flex, useToast, Input, Select,
 } from '@chakra-ui/react';
 import NextLink from 'next/link';
 import BaseLayout from 'components/Layouts/Base';
-import { useEffect, useState } from 'react';
+import { useEffect, useState, useMemo } from 'react';
 import type { UserWithTicketInfo } from 'types/user';
 import UserCard from 'components/Admin/UserCard';
+
+type SelectTypes = '' | 'has_ticket' | 'checked_in';
 
 const Home: NextPage = () => {
   const [users, setUsers] = useState<UserWithTicketInfo[] | null>(null);
   const toast = useToast();
   const [search, setSearch] = useState('');
+  const [type, setType] = useState<SelectTypes>('');
 
   useEffect(() => {
     const getUsers = async () => {
@@ -29,6 +32,27 @@ const Home: NextPage = () => {
     getUsers();
   }, [toast]);
 
+  const filtered = useMemo(() => {
+    if (!users) return null;
+    if (!search && !type) return users;
+    const searchLower = search.toLowerCase();
+    return users.filter((user) => {
+      let typeCheck: null | boolean = null;
+      switch (type) {
+        case 'has_ticket':
+          typeCheck = user.has_ticket && !user.checked_in;
+          break;
+        case 'checked_in':
+          typeCheck = user.checked_in;
+          break;
+        default:
+          typeCheck = true;
+      }
+      const searchCheck = user.name.toLowerCase().includes(searchLower);
+      return typeCheck && searchCheck;
+    });
+  }, [search, type, users]);
+
   return (
     <BaseLayout isLoading={!users}>
       <Flex direction="column" align="center">
@@ -44,10 +68,17 @@ const Home: NextPage = () => {
           </NextLink>
         </Flex>
 
-        <Input borderColor="gray.500" focusBorderColor="gray.600" _hover={{ borderColor: 'gray.600' }} minW="300px" maxW="500px" w="80%" my={3} placeholder="Search by name" value={search} onChange={(e) => setSearch(e.currentTarget.value)} />
+        <Flex direction="row" justify="center" my={5} minW="300px" maxW="700px" w="80%" gap={2}>
+          <Input borderColor="gray.500" focusBorderColor="gray.600" _hover={{ borderColor: 'gray.600' }} minW="150px" maxW="500px" w="65%" placeholder="Search by name" value={search} onChange={(e) => setSearch(e.currentTarget.value)} />
+          <Select borderColor="gray.500" focusBorderColor="gray.600" _hover={{ borderColor: 'gray.600' }} minW="150px" maxW="500px" w="35%" placeholder="Select Status" value={type} onChange={(e) => setType(e.currentTarget.value as SelectTypes)}>
+            <option value="no">No Ticket</option>
+            <option value="has_ticket">Has Ticket (Not CI)</option>
+            <option value="checked_in">Checked In</option>
+          </Select>
+        </Flex>
 
         <Flex gap={3} flexFlow="row wrap" justify="center">
-          {users?.filter((user) => user.name.toLowerCase().includes(search.toLowerCase())).map((user) => (
+          {filtered.map((user) => (
             <UserCard user={user} />
           ))}
         </Flex>

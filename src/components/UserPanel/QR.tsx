e57e817b0e110320a432d991e1c0eb5393c7003d
Nick <hello@nicholasg.me>
2022-03-15 23:34:28 +0000

    fix: lint

--- src/components/UserPanel/QR.tsx
+++ src/components/UserPanel/QR.tsx
@@ -7,7 +7,7 @@ interface Props {
 
 const QR = ({ jwt }: Props) => (
   <Box bgColor="white" border="3px solid" borderColor="brand.kimberley" w="300px" h="300px" boxSizing="content-box" borderRadius="15px" p={6}>
-    <QRCode value={jwt} size={300} fgColor="#711368" level="L" imageSettings={{src:"/kimberley_logo.png"}} />
+    <QRCode value={jwt} size={300} fgColor="#711368" level="L" imageSettings={{ src: '/kimberley_logo.png' }} />
   </Box>
 );
 

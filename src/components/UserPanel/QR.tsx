7f77268572a052801ff879b7143894545ace04fa
Modlizard <58074763>
2022-03-15 23:41:17 +0000

    Spaces

--- src/components/UserPanel/QR.tsx
+++ src/components/UserPanel/QR.tsx
@@ -7,7 +7,7 @@ interface Props {
 
 const QR = ({ jwt }: Props) => (
   <Box bgColor="white" border="3px solid" borderColor="brand.kimberley" w="300px" h="300px" boxSizing="content-box" borderRadius="15px" p={6}>
-    <QRCode value={jwt} size={300} fgColor="#711368" level="H" imageSettings={{src:"/kimberley_logo.png", height:100, width:100}} />
+    <QRCode value={jwt} size={300} fgColor="#711368" level="H" imageSettings={{ src:"/kimberley_logo.png", height:100, width:100 }} />
   </Box>
 );
 

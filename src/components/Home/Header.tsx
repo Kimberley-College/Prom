4641239ecd725aae7dcd75f0166d0f9b6eda2490
Jacques <jamie.whiting2003@gmail.com>
2022-03-19 15:28:55 +0000

    added a passHref

--- src/components/Home/Header.tsx
+++ src/components/Home/Header.tsx
@@ -20,7 +20,7 @@ const Header: React.FC = () => {
         {/* eslint-disable-next-line no-nested-ternary */}
         {isLoading ? <Spinner />
           : user ? (
-            <NextLink href="/panel"><Button leftIcon={<Icon as={FaRegUser} color="black" boxSize={8} />} fontSize="24" p={8}>User Panel</Button></NextLink>
+            <NextLink href="/panel" passHref><Button leftIcon={<Icon as={FaRegUser} color="black" boxSize={8} />} fontSize="24" p={8}>User Panel</Button></NextLink>
           )
             : (
               <Button onClick={signin} leftIcon={<Icon as={SiMicrosoftoffice} color="black" boxSize={8} />} fontSize="24" p={8}>Sign in with Office</Button>

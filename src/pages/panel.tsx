a7191403023c89459ec6b861f5bc0b8a004f9f21
Nick <hello@nicholasg.me>
2022-03-20 19:16:54 +0000

    fix: centre loading spinner on user panel (#145)

--- src/pages/panel.tsx
+++ src/pages/panel.tsx
@@ -3,14 +3,14 @@ import HasTicket from 'components/UserPanel/HasTicket';
 import NoTicket from 'components/UserPanel/NoTicket';
 import BaseLayout from 'components/Layouts/Base';
 import { useTicket } from 'util/ticketContext';
-import { Spinner, Heading } from '@chakra-ui/react';
+import { Spinner, Heading, Center } from '@chakra-ui/react';
 
 const Panel: NextPage = () => {
   const { ticket, isLoading } = useTicket();
   return (
     <BaseLayout>
       <Heading as="h1" size="3xl" textAlign="center">User Panel</Heading>
-      {isLoading ? <Spinner /> : ticket ? <HasTicket /> : <NoTicket />}
+      {isLoading ? <Center><Spinner size="xl" /></Center> : ticket ? <HasTicket /> : <NoTicket />}
     </BaseLayout>
   );
 };

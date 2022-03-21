432e3d033bf69fc5a92f14ef172898f318e776ad
Nick <33908215+NicholasG04@users.noreply.github.com>
2022-03-21 11:01:17 +0000

    feat: colour of user card now changes based on ticket status (#157)

--- src/components/Admin/UserCard.tsx
+++ src/components/Admin/UserCard.tsx
@@ -6,7 +6,7 @@ interface Props {
 }
 
 const UserCard: React.FC<Props> = ({ user }) => (
-  <Flex h="100%" w="310px" p={3} bgColor="purple.300" borderRadius={10} direction="column">
+  <Flex h="100%" w="310px" p={3} bgColor={user.checked_in ? 'green.400' : user.has_ticket ? 'blue.300' : 'purple.300'} borderRadius={10} direction="column">
     <Heading as="h3" size="lg" textAlign="center" pb={2}>{user.name}</Heading>
     <Text>Email: {user.email}</Text>
     <Text>Has Ticket: {user.has_ticket ? 'Yes' : 'No'}</Text>

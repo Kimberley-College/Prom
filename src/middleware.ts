e17540f2e5a991b3a8f0eacff98cd9155b1d5ef3
Nick <hello@nicholasg.me>
2022-07-07 22:08:00 +0100

    chore: fix work on middleware (auth broken)

--- src/middleware.ts
+++ src/middleware.ts
@@ -4,12 +4,16 @@ import type { NextRequest } from 'next/server';
 export const middleware = async (req: NextRequest) => {
   const token = req.cookies['sb-access-token'];
 
+  console.log(token);
+
   const user = await fetch(`${process.env.NEXT_PUBLIC_SUPABASE_URL}/auth/v1/user`, {
     headers: {
       Authorization: `Bearer ${token}`,
       APIKey: process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY,
     },
   }).then((res) => res.json());
+
+  console.log(user);
 
   if (req.nextUrl.pathname.startsWith('/security')) {
     if (user?.user_metadata?.admin || user?.user_metadata?.roles.includes('admin') || user?.user_metadata?.roles.includes('security')) {
